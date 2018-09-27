bb22e65c11ee6e7195dc873205883a022f09c7d0
xianghugui <arnoldlve@icloud.com>
2018-09-27 14:22:05 +0800

    update

--- config-client/src/main/java/com/icloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/icloud/configclient/ConfigClientApplication.java
@@ -3,12 +3,22 @@ package com.icloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
+
+    /**
+     * http://localhost:8881/actuator/bus-refresh
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
@@ -16,8 +26,10 @@ public class ConfigClientApplication {
 
     @Value("${foo}")
     String foo;
+
     @RequestMapping(value = "/hi")
     public String hi(){
         return foo;
     }
 }
+
