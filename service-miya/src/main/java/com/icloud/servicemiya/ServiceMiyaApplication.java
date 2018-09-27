a13093d3596b50d235cffdb9107860fee76f8030
xianghugui <arnoldlve@icloud.com>
2018-09-27 14:55:50 +0800

    update

--- service-miya/src/main/java/com/icloud/servicemiya/ServiceMiyaApplication.java
+++ service-miya/src/main/java/com/icloud/servicemiya/ServiceMiyaApplication.java
@@ -1,4 +1,4 @@
-package com.icloud.servicehi;
+package com.icloud.servicemiya;
 
 import brave.sampler.Sampler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,19 +8,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
-public class ServiceHiApplication {
+public class ServiceMiyaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceHiApplication.class, args);
+        SpringApplication.run(ServiceMiyaApplication.class, args);
     }
 
-    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+    private static final Logger LOG = Logger.getLogger(ServiceMiyaApplication.class.getName());
 
+
+    @RequestMapping("/hi")
+    public String home(){
+        LOG.log(Level.INFO, "hi is being called");
+        return "hi i'm miya!";
+    }
+
+    @RequestMapping("/miya")
+    public String info(){
+        LOG.log(Level.INFO, "info is being called");
+        return restTemplate.getForObject("http://localhost:8988/info",String.class);
+    }
 
     @Autowired
     private RestTemplate restTemplate;
@@ -30,25 +43,10 @@ public class ServiceHiApplication {
         return new RestTemplate();
     }
 
-    @RequestMapping("/hi")
-    public String callHome(){
-        LOG.log(Level.INFO, "calling trace service-hi  ");
-        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
-    }
-    @RequestMapping("/info")
-    public String info(){
-        LOG.log(Level.INFO, "calling trace service-hi ");
-
-        return "i'm service-hi";
-
-    }
 
     @Bean
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
     }
-
-
 }
-
 
